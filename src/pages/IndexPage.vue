ce93553f8871b2ce072c1eeed3f0ab571e17b440
Éric Ivan João António <ericantoniodev@gmail.com>
2022-10-08 13:26:59 +0200

    Update IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,30 @@
 <template>
   <q-page class="flex column" id="q-page1">
 
+
+    <div class="col" q-pt-lg q-px-md>
+
+      <q-input
+        class="input01"
+        v-model="text"
+        placeholder="Search"
+        borderless
+        dark>
+
+        <template v-slot:before>
+          <q-icon name="event" />
+        </template>
+
+        <template v-slot:hint>
+          Field hint
+        </template>
+
+        <template v-slot:append>
+          <q-btn round dense flat icon="add" />
+        </template>
+      </q-input>
+    </div>
+
   </q-page>
 </template>
 
@@ -17,8 +41,11 @@ export default defineComponent({
 
 <style lang="sass">
   #q-page1
+    padding-top: 10px
     background: linear-gradient(to bottom, #267871, #136a8a)
 
+  .input01
+    margin-left:10px
 
 
 </style>
